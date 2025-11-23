439d82b9e3d7b76d40aa6ba5d3cd22f85a91a995
dionae <zguixmz@gmail.com>
2025-11-23 12:24:13 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="flex align-middle justify-center h-screen items-center">
+      <h1 className="text-center text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
